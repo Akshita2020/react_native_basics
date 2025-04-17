2deca838bfb400223716c33875ce5ab4aa5b3e3f
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-17 11:13:42 +0530

    Text in css

--- App.tsx
+++ App.tsx
@@ -1,44 +1,20 @@
-import {Text, SafeAreaView, View} from 'react-native';
+import {Text, SafeAreaView} from 'react-native';
 
 function App() {
   return (
     <SafeAreaView>
-      <View style={{position: 'relative', backgroundColor: 'red'}}>
-        <View
-          style={{
-            backgroundColor: 'yellow',
-            margin: 10,
-            marginTop: 50,
-            marginLeft: 100,
-            marginRight: 100,
-            marginBottom: 50,
-            marginVertical: 50,
-            marginHorizontal: 50,
-            padding: 10,
-            paddingTop: 100,
-            paddingBottom: 100,
-            paddingLeft: 100,
-            paddingRight: 50,
-            paddingHorizontal: 50,
-            paddingVertical: 50,
-            position: 'relative',
-            zIndex: 1,
-          }}>
-          <Text> Hello world!</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: 'green',
-            position: 'absolute',
-            top: 50,
-            left: 10,
-            bottom: 10,
-            right: 20,
-            zIndex: 2,
-          }}>
-          <Text> Hello world2!</Text>
-        </View>
-      </View>
+      <Text
+        style={{
+          color: 'red',
+          fontSize: 42,
+          fontFamily: 'Arial',
+          fontStyle: 'italic',
+          fontWeight: '700',
+          // letterSpacing: 10,
+          textAlign: 'center',
+        }}>
+        Hello, world!
+      </Text>
     </SafeAreaView>
   );
 }
