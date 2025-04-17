fec8014c438779488c7d5de159028b03634119c1
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-17 13:46:23 +0530

    View  in react-native

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 import MyText from './components/MyText/Text';
 
 function App() {
   return (
     <SafeAreaView>
-      <MyText />
+      <View style={{backgroundColor: 'yellow', height: 30, alignItems:'center'}}>
+        <Text>this is going go to be my header container</Text>
+      </View>
+      <View>
+        <MyText />
+        <MyText />
+        <MyText />
+      </View>
+      <View>
+        <Text>this is going go to be my footer container</Text>
+      </View>
     </SafeAreaView>
   );
 }
--- components/MyText/Text.jsx
+++ components/MyText/Text.jsx
@@ -4,7 +4,6 @@ import styles from './styles';
 const MyText = () => {
   return (
     <Text style={[styles.text, styles.text1, {color: 'magenta'}]}>
-      {' '}
       Hello! React native world!
     </Text>
   );
