21a7143755afdd2b8dce49f431420fc8436e94e5
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-18 13:20:31 +0530

    Class based component versus functional component

--- App.tsx
+++ App.tsx
@@ -1,54 +1,13 @@
-import React, {Component} from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import MyText from './components/MyText/Text';
 
-interface AppState {
-  name?: string;
-}
-
-class App extends Component<{}, AppState> {
-  constructor(props: string) {
-    super(props);
-    this.state = {
-      name: undefined,
-    };
-  }
-  componentDidMount(): void {
-    console.log('components is mounted');
-  }
-  componentShouldMount(
-    nextProps: Readonly<{}>,
-    nextState: Readonly<{}>,
-    nextContext: any,
-  ): boolean {
-    console.log('Checking if component should update');
-    return true;
-  }
-  getSnapshotBeforeUpdate(prevProps: Readonly<{}>, prevState: Readonly<{}>) {
-    console.log('Getting snapshot before update');
-    return null;
-  }
-  componentDidUpdate(
-    prevProps: Readonly<{}>,
-    prevState: Readonly<{}>,
-    snapshot: any,
-  ) {
-    console.log('Component did update');
-  }
-  componentWillUnmount(): void {
-    console.log('Component will unmount');
-  }
-  render() {
-    return (
-      <SafeAreaView>
-        <Text
-          onPress={() => {
-            this.setState({name: 'akshita'});
-          }}>
-          Hello, {this.state?.name}!
-        </Text>
-      </SafeAreaView>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <SafeAreaView>
+      <MyText />
+      <MyText name={'Akshita'} />
+    </SafeAreaView>
+  );
+};
 export default App;
--- components/MyText/Text.jsx
+++ components/MyText/Text.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
 import {Text} from 'react-native';
+import React, {useState, useEffect, Component} from 'react';
 import styles from './styles';
-const MyText = () => {
-  const handleTextClick = () => {
-    alert('Text has been clicked');
-  };
-  return (
-    <Text
-      style={[styles.text, styles.text1, {color: 'magenta'}]}
-      onPress={() => handleTextClick()}>
-      Hello! React native world!
-    </Text>
-  );
-};
+class MyText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: props.name + ' Tiwari',
+    };
+  }
+  componentDidMount() {
+    //It is called right after the component is rendered
+    //Perform some setup for example fetching data from API
+    console.log('Our component has been mounted');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    //Called when state or props of the application is updated
+    console.log(
+      'The state of the application has been changed',
+      prevState,
+      this.state,
+    );
+  }
+  componentWillUnmount() {
+    console.log('Component is now being destroyed');
+  }
+  render() {
+    return (
+      <Text
+        style={[styles.text, styles.text1, {color: 'green'}]}
+        onPress={() =>
+          this.setState({...this.state, ...{fullName: 'Akshita, T.'}})
+        }>
+        Hello, {this.props.name}! I know that your full name is{' '}
+        {this.state.fullName}
+      </Text>
+    );
+  }
+}
 
 export default MyText;
