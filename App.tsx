a6da4e34f85c0967d9f4e95b8d7910b954ef1e28
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-18 14:49:43 +0530

    textInput component in react-native

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import MyText from './components/MyText/Text';
+import {SafeAreaView, TextInput} from 'react-native';
 
 const App = () => {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
   return (
     <SafeAreaView>
-      <MyText />
-      <MyText name={'Akshita'} />
+      <TextInput
+        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
+        value={name}
+        onChangeText={name => {
+          console.log('name:>>', name);
+          setName(name);
+        }}
+        placeholder={'Please enter your name'}
+        autoFocus={true}
+        returnKeyType={'next'}
+      />
+      <TextInput
+        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
+        value={email}
+        onChangeText={email => {
+          console.log('email:>>', email);
+          setEmail(email);
+        }}
+        placeholder={'Please enter your email'}
+        // autoFocus={true}
+        keyboardType={'email-address'}
+        returnKeyType={'next'}
+      />
+      <TextInput
+        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
+        value={password}
+        onChangeText={password => {
+          console.log('password:>>', password);
+          setPassword(password);
+        }}
+        placeholder={'Please enter password'}
+        // autoFocus={true}
+        secureTextEntry={true}
+        keyboardType={'phone-pad'}
+        returnKeyType={'go'}
+      />
     </SafeAreaView>
   );
 };
+
 export default App;
--- components/MyText/Text.jsx
+++ components/MyText/Text.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import {SafeAreaView, Image} from 'react-native';
+import {Text} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
 
-const App = () => {
-  const [image, setImage] = React.useState('http://example.com');
-  return (
-    <SafeAreaView>
-      <Image
-        source={image}
-        style={{width: 200, height: 100, backgroundColor: 'red'}}
-        resizeMode={'center'}
-        onError={() => {
-          console.log('Error has been detected while loading an image');
-          setImage(require('../../assets/cake.png'));
-        }}
-      />
-      <Image
-        source={{
-          uri: 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1089&q=80',
-        }}
-        style={{width: 200, height: 100, backgroundColor: 'blue'}}
-        resizeMode={'center'}
-      />
-    </SafeAreaView>
-  );
+const MyText = ({text}) => {
+  return <Text style={styles.text}>{text}</Text>;
+};
+MyText.propTypes = {
+  text: PropTypes.string.isRequired,
 };
 
-export default App;
+export default MyText;
