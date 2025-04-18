d1ebf08722f98add02f059709d73377ad7fef9b2
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-18 15:30:40 +0530

    Button, pressable component in react-native

--- App.tsx
+++ App.tsx
@@ -1,48 +1,33 @@
 import React from 'react';
-import {SafeAreaView, TextInput} from 'react-native';
+import {Image, SafeAreaView, ScrollView} from 'react-native';
 
 const App = () => {
-  const [name, setName] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [email, setEmail] = React.useState('');
   return (
     <SafeAreaView>
-      <TextInput
-        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
-        value={name}
-        onChangeText={name => {
-          console.log('name:>>', name);
-          setName(name);
-        }}
-        placeholder={'Please enter your name'}
-        autoFocus={true}
-        returnKeyType={'next'}
-      />
-      <TextInput
-        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
-        value={email}
-        onChangeText={email => {
-          console.log('email:>>', email);
-          setEmail(email);
-        }}
-        placeholder={'Please enter your email'}
-        // autoFocus={true}
-        keyboardType={'email-address'}
-        returnKeyType={'next'}
-      />
-      <TextInput
-        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
-        value={password}
-        onChangeText={password => {
-          console.log('password:>>', password);
-          setPassword(password);
-        }}
-        placeholder={'Please enter password'}
-        // autoFocus={true}
-        secureTextEntry={true}
-        keyboardType={'phone-pad'}
-        returnKeyType={'go'}
-      />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        horizontal={true}
+        contentContainerStyle={{backgroundColor: 'yellow', height: 600}}
+        onScroll={() => {
+          console.log('we are scrolling');
+        }}>
+        <Image
+          source={require('./assets/cake.png')}
+          style={{width: 500, height: 500, margin: 10}}
+        />
+        <Image
+          source={require('./assets/cake.png')}
+          style={{width: 500, height: 500, margin: 10}}
+        />
+        <Image
+          source={require('./assets/cake.png')}
+          style={{width: 500, height: 500, margin: 10}}
+        />
+        <Image
+          source={require('./assets/cake.png')}
+          style={{width: 500, height: 500, margin: 10}}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ components/textInput/TextInput.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {SafeAreaView, TextInput} from 'react-native';
+
+const textInput = () => {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
+        value={name}
+        onChangeText={name => {
+          console.log('name:>>', name);
+          setName(name);
+        }}
+        placeholder={'Please enter your name'}
+        autoFocus={true}
+        returnKeyType={'next'}
+      />
+      <TextInput
+        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
+        value={email}
+        onChangeText={email => {
+          console.log('email:>>', email);
+          setEmail(email);
+        }}
+        placeholder={'Please enter your email'}
+        // autoFocus={true}
+        keyboardType={'email-address'}
+        returnKeyType={'next'}
+      />
+      <TextInput
+        style={{borderWidth: 1, padding: 10, margin: 10, borderRadius: 20}}
+        value={password}
+        onChangeText={password => {
+          console.log('password:>>', password);
+          setPassword(password);
+        }}
+        placeholder={'Please enter password'}
+        // autoFocus={true}
+        secureTextEntry={true}
+        keyboardType={'phone-pad'}
+        returnKeyType={'go'}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default textInput;
