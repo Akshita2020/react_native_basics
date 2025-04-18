1a578e1d4f680778de52254d0cb6733874de37e9
Akshita2020 <akshita.tiwari2020@gmail.com>
2025-04-18 11:31:22 +0530

    Lifecycle Methods

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
 import React, {Component} from 'react';
 import {Text, SafeAreaView} from 'react-native';
 
-class App extends Component {
+interface AppState {
+  name?: string;
+}
+
+class App extends Component<{}, AppState> {
   constructor(props: string) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: undefined,
+    };
+  }
+  componentDidMount(): void {
+    console.log('components is mounted');
+  }
+  componentShouldMount(
+    nextProps: Readonly<{}>,
+    nextState: Readonly<{}>,
+    nextContext: any,
+  ): boolean {
+    console.log('Checking if component should update');
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps: Readonly<{}>, prevState: Readonly<{}>) {
+    console.log('Getting snapshot before update');
+    return null;
+  }
+  componentDidUpdate(
+    prevProps: Readonly<{}>,
+    prevState: Readonly<{}>,
+    snapshot: any,
+  ) {
+    console.log('Component did update');
+  }
+  componentWillUnmount(): void {
+    console.log('Component will unmount');
   }
   render() {
     return (
@@ -13,8 +44,7 @@ class App extends Component {
           onPress={() => {
             this.setState({name: 'akshita'});
           }}>
-          {' '}
-          Hello World{' '}
+          Hello, {this.state?.name}!
         </Text>
       </SafeAreaView>
     );
